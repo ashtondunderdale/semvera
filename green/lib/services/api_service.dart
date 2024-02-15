da0b24e4146ed9eb0ffd27f7653ed34cd82723dc
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-02-15 22:46:53 +0000

    added timestamp body param

--- green/lib/services/api_service.dart
+++ green/lib/services/api_service.dart
@@ -1,15 +1,14 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 class APIService {
 
   void addPreLaunchEmail(String email) async {
     try {
-      await http.post(Uri.parse("https://localhost:7164/Green/AddPreLaunchEmail"),
+      http.Response response = await http.post(Uri.parse("https://localhost:7164/Green/AddPreLaunchEmail"),
         headers: <String, String>{'Content-Type': 'application/json; charset=UTF-8'},
-        body: jsonEncode({"email":email})
-      );
+        body: jsonEncode({"emailAddress": email, "timeStamp": DateTime.now().toString()})
+      );      
 
     } catch (exception) {
       print(exception);
